9e448fc1abea2889877160c64effe66c61187afd
Albert Wang <git@albertyw.com>
2025-02-16 19:32:30 -0800

    Initialize ARIMA

--- goarima.go
+++ goarima.go
@@ -1,1 +1,25 @@
 package goarima
+
+// TimeSeries represents a time series of data for ARIMA to operate on
+type TimeSeries struct {
+	Data []float64
+}
+
+// ARIMA is the configuration of a model which operates on a TimeSeries
+type ARIMA struct {
+	// The order of the AR model
+	p int
+	// The order of the I model
+	d int
+	// The order of the MA model
+	q int
+}
+
+// NewARIMA creates a new ARIMA model with the given parameters
+func NewARIMA(p, d, q int) ARIMA {
+	return ARIMA{
+		p: p,
+		d: d,
+		q: q,
+	}
+}
