f79555a4144a210f4b01c8b6bcc2276b2e7aa341
Albert Wang <git@albertyw.com>
2025-08-10 01:53:20 -0700

    Add simple arima test cases

--- goarima_test.go
+++ goarima_test.go
@@ -7,6 +7,56 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestSimpleARIMA(t *testing.T) {
+	var testCases = []struct {
+		name     string
+		data     []float64
+		p, d, q  int
+		expected []float64
+	}{
+		{
+			name:     "ARIMA(1,0,0) with simple data",
+			data:     []float64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+			p:        1,
+			d:        0,
+			q:        0,
+			expected: []float64{11, 12, 13, 14, 15},
+		},
+		{
+			name:     "ARIMA(1,1,1) with simple data",
+			data:     []float64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+			p:        1,
+			d:        1,
+			q:        1,
+			expected: []float64{11, 12, 13, 14, 15},
+		},
+		{
+			name:     "ARIMA(1,0,0) with simple data",
+			data:     []float64{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+			p:        1,
+			d:        0,
+			q:        0,
+			expected: []float64{1, 1, 1, 1, 1},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			model, err := NewARIMA(tc.p, tc.d, tc.q)
+			require.NoError(t, err)
+			require.NotNil(t, model)
+			err = model.Fit(tc.data)
+			require.NoError(t, err)
+			forecast, err := model.Forecast(5)
+			require.NoError(t, err)
+			assert.Equal(t, len(forecast), 5)
+			for i := range forecast {
+				assert.InDelta(t, forecast[i], tc.expected[i], 1e-6)
+			}
+		})
+	}
+}
+
 func TestARIMA(t *testing.T) {
 	data := []float64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 	p, d, q := 1, 1, 1
