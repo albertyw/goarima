018dd404fdceabaa4ed2c3ac4c4a3c6b771193ad
Albert Wang <git@albertyw.com>
2025-08-10 02:00:01 -0700

    Refactor generate_pmdarima

--- example/generate_pmdarima.py
+++ example/generate_pmdarima.py
@@ -18,6 +18,31 @@ from pmdarima import ARIMA
 DATA_DIRECTORY = "data/*.csv"
 
 
+def predict_arima(data: list[float], periods: int = 12) -> list[float]:
+    # Fit an ARIMA model to the data
+    model = ARIMA(order=(1, 0, 0), seasonal_order=(0, 0, 0, 0))
+    model.fit(data)
+
+    # Output model data and predictions
+    print("\033[95mParams\033[0m")
+    print(model.get_params())
+
+    print("\033[95mModel Summary\033[0m")
+    print(model.summary())
+
+    # print("\033[95mModel Dict\033[0m")
+    # print(model.to_dict())
+
+    print("\033[95mAR Coefficients (Phi)\033[0m")
+    print(model.arparams())
+
+    print("\033[95mMA Coefficients (Theta)\033[0m")
+    print(model.maparams())
+
+    print("\033[95mPredictions\033[0m")
+    print(model.predict(n_periods=12))
+
+
 def main() -> None:
     # Get all CSV files in the data directory
     files = glob.glob(DATA_DIRECTORY)
@@ -27,29 +52,8 @@ def main() -> None:
         # Read the CSV file into a DataFrame
         with open(file) as handle:
             data = handle.readlines()
-
-        # Fit an ARIMA model to the data
-        model = ARIMA(order=(1, 0, 0), seasonal_order=(0, 0, 0, 0))
-        model.fit(data)
-
-        # Output model data and predictions
-        print("\033[95mParams\033[0m")
-        print(model.get_params())
-
-        print("\033[95mModel Summary\033[0m")
-        print(model.summary())
-
-        # print("\033[95mModel Dict\033[0m")
-        # print(model.to_dict())
-
-        print("\033[95mAR Coefficients (Phi)\033[0m")
-        print(model.arparams())
-
-        print("\033[95mMA Coefficients (Theta)\033[0m")
-        print(model.maparams())
-
-        print("\033[95mPredictions\033[0m")
-        print(model.predict(n_periods=12))
+        data = [float(line.strip()) for line in data if line.strip().isdigit()]
+        predict_arima(data)
 
 
 if __name__ == "__main__":
