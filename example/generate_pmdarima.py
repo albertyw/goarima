48c291cde239ebd314d061c79a2c999d9eafe577
Albert Wang <git@albertyw.com>
2025-08-09 19:50:23 -0700

    Add a script to run ARIMA from pmdarima

---
+++ example/generate_pmdarima.py
@@ -0,0 +1,56 @@
+"""
+This script reads all of the files in the data directory,
+fits the data for PMDARIMA, and saves the results to a file.
+
+Run:
+    python3.12 -m venv env
+    env/bin/pip install pmdarima
+    env/bin/pip install 'numpy<2'
+
+Data is from https://github.com/alkaline-ml/pmdarima/tree/master/pmdarima/datasets/
+"""
+
+import glob
+
+from pmdarima import ARIMA
+
+
+DATA_DIRECTORY = "data/*.csv"
+
+
+def main() -> None:
+    # Get all CSV files in the data directory
+    files = glob.glob(DATA_DIRECTORY)
+
+    # Iterate through each file
+    for file in files:
+        # Read the CSV file into a DataFrame
+        with open(file) as handle:
+            data = handle.readlines()
+
+        # Fit an ARIMA model to the data
+        model = ARIMA(order=(1, 1, 1))
+        model.fit(data)
+
+        # Output model data and predictions
+        print("\033[95mParams\033[0m")
+        print(model.get_params())
+
+        print("\033[95mModel Summary\033[0m")
+        print(model.summary())
+
+        print("\033[95mModel Dict\033[0m")
+        print(model.to_dict())
+
+        print("\033[95mAR Coefficients (Phi)\033[0m")
+        print(model.arparams())
+
+        print("\033[95mMA Coefficients (Theta)\033[0m")
+        print(model.maparams())
+
+        print("\033[95mPredictions\033[0m")
+        print(model.predict(n_periods=10))
+
+
+if __name__ == "__main__":
+    main()
